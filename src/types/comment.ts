fbaffc5ed0e640adde55b60554696ea994717652
Teerapat <email@github.com>
2026-07-01 09:27:22 +0700

    chore(types): update comment types

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -2,15 +2,19 @@
 
 export type Reply = {
 	id: number;
+	userId: string;
 	name: string;
 	username: string;
 	date: string;
 	avatar: string;
 	text: string;
+	likes: number;
+	likedByMe: boolean;
 };
 
 export type Comment = {
 	id: number;
+	userId: string;
 	name: string;
 	username: string;
 	date: string;
